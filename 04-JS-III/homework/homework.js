1e25f26663fe5d6d1e33685f954ee1790be1cb42
unknown <xA6V5x>
2021-12-24 20:52:39 -0300

    numero mas grande

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -102,7 +102,7 @@ function numeroMasGrande(numeros) {
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
+  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
 }
 
