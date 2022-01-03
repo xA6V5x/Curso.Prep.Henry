e887e1a981f36dec4fb69c96b4d4b9fb27ea43ce
unknown <xA6V5x>
2022-01-02 23:45:51 -0300

    04-JS-III finish

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -271,7 +271,7 @@ function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
   //Devolver el array
-  //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
+  //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteracción
   //Pista: usá el statement 'continue'
   // Tu código:
   var array = [];
